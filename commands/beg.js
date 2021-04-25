f35243ef710b89f8b892099d15dab94b85d8901f
SrPierryON <81433543+SrPierryON@users.noreply.github.com>
2021-04-25 17:14:44 -0300

    Update beg.js

--- commands/beg.js
+++ commands/beg.js
@@ -3,29 +3,29 @@ const db = require("quick.db");
 const ms = require("parse-ms");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.content.startsWith('m!'))return;  
+  if(!message.content.startsWith('s.'))return;  
 
   let user = message.author;
 
   let timeout = 180000;
   let amount = 5;
 
-  let beg = await db.fetch(`beg_${message.guild.id}_${user.id}`);
+  let beg = await db.fetch(`beg_${user.id}`);
 
   if (beg !== null && timeout - (Date.now() - beg) > 0) {
     let time = ms(timeout - (Date.now() - beg));
   
     let timeEmbed = new Discord.RichEmbed()
-    .setColor("#FFFFFF")
-    .setDescription(`<:Cross:618736602901905418> You've already begged recently\n\nBeg again in ${time.minutes}m ${time.seconds}s `);
+    .setColor("#2f3136")
+    .setDescription(`**<:s_err:835971194887864342>・Você já implorou recentemente, Implore novamente em ${time.minutes} minutos e ${time.seconds} segundos**`);
     message.channel.send(timeEmbed)
   } else {
     let moneyEmbed = new Discord.RichEmbed()
   .setColor("#FFFFFF")
-  .setDescription(`<:Check:618736570337591296> You've begged and received ${amount} coins`);
+  .setDescription(`<:s_yes:835971288118853664> **|** Você implorou e recebeu ${amount} coins`);
   message.channel.send(moneyEmbed)
-  db.add(`money_${message.guild.id}_${user.id}`, amount)
-  db.set(`beg_${message.guild.id}_${user.id}`, Date.now())
+  db.add(`money_${user.id}`, amount)
+  db.set(`beg_${user.id}`, Date.now())
 
 
   }
@@ -34,5 +34,5 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name:"beg",
-  aliases: [""]
+  aliases: ["implorar", "mendigar"]
 }
